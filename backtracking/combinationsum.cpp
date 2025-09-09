47e7e8ee6258581874ae50428789a4e5e7d6ce3d
salmanibnabubaker <128457831+salmanibnabubaker@users.noreply.github.com>
2025-09-09 16:57:09 +0000

    solved combinationsum.cpp

---
+++ backtracking/combinationsum.cpp
@@ -0,0 +1,27 @@
+void findSum(int sum,int i,vector<int>&arr,vector<int>&curr,vector<vector<int>>&combs){
+        if(sum<0){
+            return;
+        }
+        if(sum==0){
+            combs.push_back(curr);
+            return;
+        }
+        for(int j=i;j<arr.size();j++){
+            curr.push_back(arr[j]);
+            findSum(sum-arr[j],j,arr,curr,combs);
+            curr.pop_back();
+        }
+        return;
+    }
+    
+    vector<vector<int>> combinationSum(vector<int> &arr, int target) {
+        // Your code here
+        vector<int>curr;
+        vector<vector<int>>combs;
+        for(int i=0;i<arr.size();i++){
+            curr.push_back(arr[i]);
+            findSum(target-arr[i],i,arr,curr,combs);
+            curr.pop_back();
+        }
+        return combs;
+    }
