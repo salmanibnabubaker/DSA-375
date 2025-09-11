46762cfc0451006675870ed151ae2e19d88a7636
salmanibnabubaker <128457831+salmanibnabubaker@users.noreply.github.com>
2025-09-11 09:37:51 +0000

    added bottom-up in lis.cpp

--- dp/lis.cpp
+++ dp/lis.cpp
@@ -1,3 +1,4 @@
+//memoization TC-O(N^2) SC-O(N)
 int increasinglength(int index,int prev,vector<int>&arr,vector<int>&memo){
     if(prev>=arr[index]){
         return 0;
@@ -20,4 +21,28 @@ int lis(vector<int>& arr) {
         maxlen=max(maxlen,increasinglength(i,-1,arr,memo));
     }
     return maxlen;
+}
+
+
+//bottom-up TC-O(N^2) SC-O(N)
+int lis(vector<int>& arr) {
+    // code here
+    vector<int>longest(arr.size(),1);
+    for(int i=1;i<arr.size();i++){
+        int maxlen=0;
+        for(int j=0;j<i;j++){
+            if(arr[i]>arr[j]&&longest[j]>maxlen){
+                maxlen=longest[j];
+            }
+        }
+        longest[i]=maxlen+1;
+    }
+    
+    int res=longest[0];
+    for(int i=1;i<longest.size();i++){
+        if(res<longest[i]){
+            res=longest[i];
+        }
+    }
+    return res;
 }
